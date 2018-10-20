e8eeb588d6390c58510495f2869f202a309f0391
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2018-10-20 13:25:16 -0700

    V.0.0.0 Intro

--- source.py
+++ source.py
@@ -8,11 +8,10 @@ Tumor locations are available
 
 2) Find CNN trained with abdomenal MRIs
     2.1) Segment images for detection of an important organ
-    2.2) determine location of the organ
+    2.2) determine location of the organ (coordinates of the object edges
 
 3) Reconstruct optimal path
 
-3)
 """
 
-
+import pandas as pd
