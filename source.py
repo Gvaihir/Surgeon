cc2a8dba02bd0844c45d0e2c61fb071bc4b0a68f
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2018-10-21 08:23:59 -0700

    V.0.0.1 using t-SNE for image clustering

--- source.py
+++ source.py
@@ -19,18 +19,52 @@ import pandas as pd
 import cv2
 import numpy as np
 import pydicom
+from os import listdir
+from os.path import isfile, join
+import os
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+import ggplot
+
 
 if __name__ == "__main__":
 
-    with open(argsP.i, 'r') as file:
+    df_all = pd.DataFrame()
 
-        df_all = pd.DataFrame()
+    root = '/Users/antonogorodnikov/Documents/Work/Python/Prostate_ADC/'
 
-        # read file
-        ds = pydicom.dcmread("/Users/antonogorodnikov/Documents/Work/Python/Prostate_ADC/9-7-ep2ddifftraDYNDISTADC-30502/000008.dcm")
+    for path, subdirs, file in os.walk(root):
+        for name in file:
+            if ".dcm" in name:
+                file_path = os.path.join(path, name)
 
-        #make a flatten np array
-        ds_flat = ds.pixel_array.flatten()
+                # read file
+                ds = pydicom.dcmread(file_path)
 
-        # add to data frame
+                # make a flatten np array
+                ds_flat = ds.pixel_array.flatten()
+
+                # add to data frame
+                df_all['/'.join(file_path.split('/')[-2:])] = ds_flat
+
+                df_all = df_all/255.0
+
+                rndperm = np.random.permutation(df_all.shape[0])
+
+                pca_100 = PCA(n_components=100)
+
+                pca_result_100 = pca_100.fit_transform(df_all.values)
+
+                print('Cumulative explained variation for 50 principal components: {}'.format(
+                    np.sum(pca_100.explained_variance_ratio_)))
+
+                time_start = time.time()
+                tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+                tsne_pca_results = tsne.fit_transform(pca_result_100[rndperm])
+
+                print
+                't-SNE done! Time elapsed: {} seconds'.format(time.time() - time_start)
+
 
